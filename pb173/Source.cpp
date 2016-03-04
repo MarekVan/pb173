46b78907472c3f0a10fc20cefb49823246c5c525
Marek <marek.kicnav@gmail.com>
2016-03-04 23:25:06 +0100

    code divided into functions, first test added

--- pb173/Source.cpp
+++ pb173/Source.cpp
@@ -1,3 +1,4 @@
+//#define _CRT_NONSTDC_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +7,148 @@
 #include "mbedtls/sha512.h"
 #include "mbedtls/cipher.h"
 #include "mbedtls/aes.h"
+
+// NIST
+
+int encrypt(FILE *input, FILE *output, unsigned char *key, unsigned char *IV) {
+	mbedtls_cipher_context_t aes_ctx;
+	mbedtls_sha512_context sha_ctx;
+	unsigned char cinput[16];
+	unsigned char coutput[16];
+	unsigned char sha_output[64];
+
+	mbedtls_cipher_setup(&aes_ctx, mbedtls_cipher_info_from_type(MBEDTLS_CIPHER_AES_128_CBC));
+	mbedtls_cipher_set_padding_mode(&aes_ctx, MBEDTLS_PADDING_PKCS7);
+
+	mbedtls_sha512_init(&sha_ctx);
+	mbedtls_sha512_starts(&sha_ctx, 0);
+
+	fseek(input, 0, SEEK_END);
+	int filesize = ftell(input);
+	rewind(input);
+
+	mbedtls_cipher_set_iv(&aes_ctx, IV, 16);
+	mbedtls_cipher_setkey(&aes_ctx, key, 128, MBEDTLS_ENCRYPT);
+
+	mbedtls_sha512_update(&sha_ctx, IV, 16);
+	fwrite(IV, 1, 16, output);
+	/*fclose(input);
+	fclose(output);
+	return 0;*/
+
+	for (int offset = 0; offset < filesize; offset += 16)
+	{
+		int n = (filesize - offset > 16) ? 16 : (int)
+			(filesize - offset);
+
+		if (fread(cinput, 1, n, input) != (size_t)n)
+		{
+			printf("fread failed\n");
+			fclose(input);
+			return 1;
+		}
+
+		mbedtls_cipher_update(&aes_ctx, cinput, n, coutput, (size_t*)&n);
+
+		mbedtls_sha512_update(&sha_ctx, cinput, n);
+
+		//mbedtls_aes_crypt_cbc(&aes_ctx, MBEDTLS_AES_ENCRYPT, n, IV, cinput, coutput);
+
+		if (fwrite(coutput, 1, n, output) != n)
+		{
+			printf("fwrite failed\n");
+			fclose(input);
+			fclose(output);
+			return 1;
+		}
+	}
+	mbedtls_sha512_finish(&sha_ctx, sha_output);
+	fwrite(sha_output, 1, 64, output);
+
+	fclose(input);
+	fclose(output);
+
+	return 0;
+}
+
+int decrypt(FILE *input, FILE *output, unsigned char *key) {
+	mbedtls_cipher_context_t aes_ctx;
+	mbedtls_sha512_context sha_ctx;
+	unsigned char cinput[16];
+	unsigned char coutput[16];
+	unsigned char sha_output[64];
+	unsigned char IV[16];
+
+	mbedtls_cipher_setup(&aes_ctx, mbedtls_cipher_info_from_type(MBEDTLS_CIPHER_AES_128_CBC));
+	mbedtls_cipher_set_padding_mode(&aes_ctx, MBEDTLS_PADDING_PKCS7);
+
+	mbedtls_sha512_init(&sha_ctx);
+	mbedtls_sha512_starts(&sha_ctx, 0);
+
+	fseek(input, 0, SEEK_END);
+	int filesize = ftell(input);
+	rewind(input);
+
+	mbedtls_cipher_setkey(&aes_ctx, key, 128, MBEDTLS_DECRYPT);
+
+	if (filesize < 16)
+	{
+		printf("file is invalid\n");
+		return 1;
+	}
+
+	fread(IV, 1, 16, input);
+	
+	mbedtls_sha512_update(&sha_ctx, IV, 16);
+	mbedtls_cipher_set_iv(&aes_ctx, IV, 16);
+
+	for (int offset = 0; offset < filesize - 64; offset += 16)
+	{
+		int n = ((filesize - 64) - offset > 16) ? 16 : (int)
+			((filesize - 64) - offset);
+
+		if (fread(cinput, 1, n, input) != (size_t)n)
+		{
+			printf("fread failed\n");
+			fclose(input);
+			fclose(output);
+			return 0;
+		}
+
+		mbedtls_cipher_update(&aes_ctx, cinput, n, coutput, (size_t*)&n);
+		mbedtls_sha512_update(&sha_ctx, cinput, n);
+
+		//mbedtls_aes_crypt_cbc(&aes_ctx, MBEDTLS_AES_DECRYPT, n, IV, cinput, coutput);
+
+		if (fwrite(coutput, 1, n, output) != n)
+		{
+			printf("fwrite failed\n");
+			fclose(input);
+			fclose(output);
+			return 1;
+		}
+	}
+
+	unsigned char control_hash[64];
+	fread(control_hash, 1, 64, input);
+
+	mbedtls_sha512_finish(&sha_ctx, sha_output);
+
+	if (memcmp(control_hash, sha_output, 64) != 0)
+	{
+		printf("wrong hash\n");
+		return 1;
+	}
+	else
+	{
+		printf("hash ok\n");
+	}
+
+	fclose(input);
+	fclose(output);
+
+	return 0;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -25,23 +168,23 @@ int main(int argc, char *argv[]) {
 
 	err = fopen_s(&input, argv[1], "rb");
 
-	if (err == 0)
+	if (err != 0)
 	{
 		printf("coudlnt open input file\n");
 		return 0;
 	}
 
-	err = fopen_s(&output, argv[1], "wb");
+	err = fopen_s(&output, argv[2], "wb");
 
-	if (err == 0)
+	if (err != 0)
 	{
 		printf("coudlnt open output file\n");
 		return 0;
 	}
 
-	err = fopen_s(&keyfile, argv[1], "rb");
+	err = fopen_s(&keyfile, argv[3], "rb");
 
-	if (err == 0)
+	if (err != 0)
 	{
 		printf("coudlnt open key file\n");
 		return 0;
@@ -52,7 +195,7 @@ int main(int argc, char *argv[]) {
 	fclose(keyfile);
 	//mbedtls_aes_setkey_enc(&aes_ctx, key, 128);
 
-	mbedtls_cipher_setup(&aes_ctx, mbedtls_cipher_info_from_type(MBEDTLS_CIPHER_AES_128_CBC));
+	/*mbedtls_cipher_setup(&aes_ctx, mbedtls_cipher_info_from_type(MBEDTLS_CIPHER_AES_128_CBC));
 	mbedtls_cipher_set_padding_mode(&aes_ctx, MBEDTLS_PADDING_PKCS7);
 
 	mbedtls_sha512_init(&sha_ctx);
@@ -60,11 +203,13 @@ int main(int argc, char *argv[]) {
 
 	fseek(input, 0, SEEK_END);
 	int filesize = ftell(input);
-	rewind(input);
+	rewind(input);*/
 
 	if (strcmp(argv[4], "-e") == 0)  // encryption
 	{
-		mbedtls_cipher_set_iv(&aes_ctx, IV, 16);
+		return encrypt(input, output, key, IV);
+		
+		/*mbedtls_cipher_set_iv(&aes_ctx, IV, 16);
 		mbedtls_cipher_setkey(&aes_ctx, key, 128, MBEDTLS_ENCRYPT);
 
 		mbedtls_sha512_update(&sha_ctx, IV, 16);
@@ -95,15 +240,16 @@ int main(int argc, char *argv[]) {
 				fclose(output);
 				return 0;
 			}
-		}
-
-		mbedtls_sha512_finish(&sha_ctx, sha_output);
-		fwrite(sha_output, 1, 64, output);
 	}
+
+	mbedtls_sha512_finish(&sha_ctx, sha_output);
+	fwrite(sha_output, 1, 64, output);*/
+}
 
 	else if (strcmp(argv[4], "-d") == 0)  // decrytion
 	{
-		if (filesize < 16)
+		decrypt(input, output, key);
+		/*if (filesize < 16)
 		{
 			printf("file is invalid\n");
 			return 0;
@@ -154,7 +300,7 @@ int main(int argc, char *argv[]) {
 		else
 		{
 			printf("hash ok\n");
-		}
+		}*/
 	}
 
 	else
---
+++ pb173/testing.cpp
@@ -0,0 +1,39 @@
+/**
+* @file main.cpp
+* @author Martin Ukrop
+* @licence MIT Licence
+*/
+
+#include "Source.cpp"
+
+// Tell CATCH to define its main function here
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Encrypt -> Decrypt testing", "") {
+	FILE *input = fopen("testDataInput1", "br+");
+	FILE *output = fopen("testDataOutput1", "bw+");
+	FILE *check = fopen("testDataCheck1", "br");
+	unsigned char key[16] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+	unsigned char IV[16]= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	encrypt(input, output, key, IV);
+	decrypt(output, input, key);
+
+	rewind(input);
+	fseek(input, 0, SEEK_END);
+	int filesize = ftell(input);
+	rewind(input);
+	unsigned char output1[128];
+	unsigned char output2[128];
+
+	for (int offset = 0; offset < filesize - filesize % 128; offset += 128)
+	{
+		fread(output1, 1, 128, input);
+		fread(output1, 1, 128, check);
+		CHECK("Comparing outputs", memcmp(output1, output2, 128) == 0);
+	}
+
+	fread(output1, 1, filesize % 128, input);
+	fread(output1, 1, filesize % 128, check);
+	CHECK("Comparing outputs", memcmp(output1, output2, filesize % 128) == 0);
+}
