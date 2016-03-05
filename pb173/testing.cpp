59973e826fc2463458055dd9b4c09de9d1b6dab8
Marek <marek.kicnav@gmail.com>
2016-03-05 12:08:02 +0100

    test of test 2

--- pb173/testing.cpp
+++ pb173/testing.cpp
@@ -10,12 +10,44 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("Encrypt -> Decrypt testing", "") {
+TEST_CASE("Encrypt -> Decrypt testing", "first") {
 	FILE *input = fopen("testDataInput1", "br+");
 	FILE *output = fopen("testDataOutput1", "bw+");
 	FILE *check = fopen("testDataCheck1", "br");
 	unsigned char key[16] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
 	unsigned char IV[16]= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	encrypt(input, output, key, IV);
+	decrypt(output, input, key);
+
+	rewind(input);
+	fseek(input, 0, SEEK_END);
+	int filesize = ftell(input);
+	rewind(input);
+	unsigned char output1[128];
+	unsigned char output2[128];
+
+	for (int offset = 0; offset < filesize - filesize % 128; offset += 128)
+	{
+		fread(output1, 1, 128, input);
+		fread(output1, 1, 128, check);
+		CHECK("Comparing outputs", memcmp(output1, output2, 128) == 0);
+	}
+
+	fread(output1, 1, filesize % 128, input);
+	fread(output1, 1, filesize % 128, check);
+	CHECK("Comparing outputs", memcmp(output1, output2, filesize % 128) == 0);
+
+	fclose(input);
+	fclose(output);
+	fclose(check);
+}
+
+TEST_CASE("Encrypt -> Decrypt testing", "second") {
+	FILE *input = fopen("testDataInput1", "br+");
+	FILE *output = fopen("testDataOutput1", "bw+");
+	FILE *check = fopen("testDataCheck1", "br");
+	unsigned char key[16] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' };
+	unsigned char IV[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	encrypt(input, output, key, IV);
 	decrypt(output, input, key);
 
